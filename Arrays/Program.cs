f53d58a7ff3b5fedf11393fda7c5e4efc3f11c9e
kgatliso <Kgatliso481@live.com>
2021-11-01 11:36:14 +0200

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] list1 = new int[] { 1, 2, 3, 4, 5 };
+            int[] list2 = new int[] { 3, 4, 5, 6, 7 };
+
+            var common = list1.Intersect(list2);
+
+            foreach (int list3 in common)
+            {
+                Console.WriteLine(string.Join(",", list3));
+            }
+
+            var list4 = list1.Except(list2);
+            Console.WriteLine(string.Join(",", list4));
+
+            var list5 = list2.Except(list1);
+            Console.WriteLine(string.Join(",", list5));
+
+            Console.WriteLine("Press <ENTER> to continue");
+
+            Console.ReadLine();
+        }
+    }
+}
